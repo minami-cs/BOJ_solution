a607b4c05cc8ede93d13bab89bfc9afc4f5a5ee6
minami-cs <mina9301@gmail.com>
2021-01-24 22:29:07 +0900

    입출력과 사칙연산 오늘은 여기까지

---
+++ step1/1000.py
@@ -0,0 +1,6 @@
+# a와 b는 정수여야 한다.
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
---
+++ step1/10869.py
@@ -0,0 +1,10 @@
+# a와 b는 자연수이고 나눗셈 결과는 정수
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
