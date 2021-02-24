dbb31787c199d8617e096aaa283cd6bb1c80bf27
minami-cs <mina9301@gmail.com>
2021-02-25 02:01:34 +0900

    1110.py 수정

--- step4/1110.py
+++ step4/1110.py
@@ -11,3 +11,5 @@ while True:
     if n == temp:
         break
 print(count)
+
+#참고:https://gabii.tistory.com/entry/BaekJoonPython3-%EB%B0%B1%EC%A4%80-1110%EB%B2%88-%EB%8D%94%ED%95%98%EA%B8%B0-%EC%82%AC%EC%9D%B4%ED%81%B4
