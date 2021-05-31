5cbbca9393cf9daafae6c663a3334b817e150796
minami-cs <mina9301@gmail.com>
2021-05-31 23:11:58 +0900

    Q. 1316: Group words check DONE

---
+++ step7/1316.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    w = input()
+    # range(len(w))이면 w.find(w[j-1])에서 w[-1]이 되므로 range(1, len(w))로 해주자.
+    for j in range(1, len(w)):
+        if w.find(w[j-1]) > w.find(w[j]):
+            n -= 1
+            # 그룹단어가 아닌 경우 계속 검사하지 않고 다음 단어를 검삭하도록 넘어가게 break를 걸어주자
+            break
+
+print(n)
