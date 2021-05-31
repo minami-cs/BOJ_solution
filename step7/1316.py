74f083acaae5936880f24f4f30d308c04024e374
minami-cs <mina9301@gmail.com>
2021-05-31 23:14:34 +0900

    1316 문제 주석 오타 수정

--- step7/1316.py
+++ step7/1316.py
@@ -6,7 +6,7 @@ for i in range(n):
     for j in range(1, len(w)):
         if w.find(w[j-1]) > w.find(w[j]):
             n -= 1
-            # 그룹단어가 아닌 경우 계속 검사하지 않고 다음 단어를 검삭하도록 넘어가게 break를 걸어주자
+            # 그룹단어가 아닌 경우 계속 검사하지 않고 다음 단어를 검사하도록 넘어가게 break를 걸어주자
             break
 
 print(n)
